4d5f49efb496143533d390de6ce917fbc009353c
mdiallo98 <mamadou1237@gmail.com>
2022-06-15 23:01:33 -0400

    got rid of the searchbar component added routing to differnt profiles for dog

--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -3,18 +3,14 @@ import React from 'react';
 function CardView() {
   let { id } = useParams();
   const [result, setResult] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
 
+  const url = 'http://eulerity-hackathon.appspot.com/pets';
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=ab166ff82684910ae3565621aea04d62&language=en-US`
-    )
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setResult(data);
       });
-
-    setIsLoading(false);
   }, [id]);
   return <div>CardView</div>;
 }
